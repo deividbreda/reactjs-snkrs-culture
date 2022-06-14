55e146382b4a4702a65d90493f6ed73e6c3c98f9
deividbreda <deivid.breda02@gmail.com>
2022-06-14 00:38:33 -0300

    Consumindo dados da API PrimisCMS através de SSG(Server Static Generation) na página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,32 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import { RichText } from "prismic-dom";
+import { getPrismicClient } from "../services/prismic";
 
 import styles from './home.module.scss'
 
-export default function Home() {
+type Product = {
+  slug: string;
+  title: string;
+  img: string;
+  excerpt: string;
+  publicatedDate: string;
+}
+
+type Noticia = {
+  slug: string;
+  title: string;
+  img: string;
+  excerpt: string;
+  publicatedDate: string;
+}
+
+interface HomeProps {
+  products: Product[],
+  news: Noticia[]
+}
+
+export default function Home({ products, news }: HomeProps) {
   return (
     <>
       <Head>
@@ -21,73 +45,83 @@ export default function Home() {
         <div className="containerWidth">
           <section className={styles.posts}>
             <div className={styles.news}>
-              <div className="col col-2">
+              {news.map(noticia => (
+                <div key={noticia.slug} className="col col-2">
                 <div className={styles.new}>
-                  <img src="/images/categoria1.jpg" alt="" />
-                  <div className={styles.cover}>
-                    <div className={styles.text}>
-                      <h1> Novo modelo slide da Adidas está sendo chamado de o novo Yeezy Slide </h1>
-                      <p> 21/06/2022 </p>
+                  <img src={noticia.img} alt="" />
+                    <div className={styles.cover}>
+                      <div className={styles.text}>
+                        <h1> {noticia.title} </h1>
+                        <p> {noticia.publicatedDate} </p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-
-              <div className="col col-2">
-                <div className={styles.new}>
-                  <img src="/images/categoria1.jpg" alt="" />
-                  <div className={styles.cover}>
-                    <div className={styles.text}>
-                      <h1> Novo modelo slide da Adidas está sendo chamado de o novo Yeezy Slide </h1>
-                      <p> 21/06/2022 </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              ))}
             </div>
 
             <div className={styles.sneakers}>
-              <div className="col col-3">
-                <a href="">
-                  <div className={styles.sneaker}>
-                    <img src="/images/categoria1.jpg" alt="" />
-                    <div className={styles.text}>
-                      <h1> "Novo" Yeezy slide? </h1>
-                      <p> Novo modelo slide da Adidas está sendo chamado de o novo Yeezy Slide </p>
-                      <span> 12/06/2022 </span>
+              {products.map(product => (
+                <div key={product.slug} className="col col-3">
+                  <a href="">
+                    <div className={styles.sneaker}>
+                      <img src={product.img} alt="" />
+                      <div className={styles.text}>
+                        <h1> {product.title} </h1>
+                        <p> {product.excerpt} </p>
+                        <span> {product.publicatedDate} </span>
+                      </div>
                     </div>
-                  </div>
-                </a>
-              </div>
-
-              <div className="col col-3">
-                <a href="">
-                  <div className={styles.sneaker}>
-                    <img src="/images/categoria1.jpg" alt="" />
-                    <div className={styles.text}>
-                      <h1> "Novo" Yeezy slide? </h1>
-                      <p> Novo modelo slide da Adidas está sendo chamado de o novo Yeezy Slide </p>
-                      <span> 12/06/2022 </span>
-                    </div>
-                  </div>
-                </a>
-              </div>
-
-              <div className="col col-3">
-                <a href="">
-                  <div className={styles.sneaker}>
-                    <img src="/images/categoria1.jpg" alt="" />
-                    <div className={styles.text}>
-                      <h1> "Novo" Yeezy slide? </h1>
-                      <p> Novo modelo slide da Adidas está sendo chamado de o novo Yeezy Slide </p>
-                      <span> 12/06/2022 </span>
-                    </div>
-                  </div>
-                </a>
-              </div>
+                  </a>
+                </div>
+              ))}
             </div>
           </section>
         </div>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+  const responseNews = await prismic.getByType("news", {
+    pageSize: 2,
+  });
+
+  const news = responseNews.results.map(noticia => {
+    return {
+      slug: noticia.uid,
+      title: RichText.asText(noticia.data.title),
+      img: noticia.data.image.url,
+      excerpt: noticia.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      publicatedDate: new Date(noticia.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    };
+  })
+
+  const responseProduct = await prismic.getByType("product", {
+    pageSize: 6,
+  });
+
+  const products = responseProduct.results.map(product => {
+    return {
+      slug: product.uid,
+      title: RichText.asText(product.data.title),
+      img: product.data.image.url,
+      excerpt: product.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      publicatedDate: new Date(product.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    };
+  })
+
+  return {
+    props: { products, news }
+  }
 }
